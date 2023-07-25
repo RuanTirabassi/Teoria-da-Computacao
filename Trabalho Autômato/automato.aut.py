47ebbba0d27dd69d7fc6f8bc614ad4c74974540e
RuanTirabassi <130397101+RuanTirabassi@users.noreply.github.com>
2023-07-24 21:39:39 -0300

    -

--- Trabalho Autômato/automato.aut.py
+++ Trabalho Autômato/automato.aut.py
@@ -1,13 +1,12 @@
 import json
 import csv
 
-with open ('arquivo_do_automato.aut.json') as arq_json:
-    arq =  json.load(arq_json)
-    
-    print(arq)
+with open('arquivo_do_automato.aut.json') as arquivo_json:
+    arq_json = json.load(arquivo_json)
 
-with open('arquivo_de_testes.in.csv', 'r') as arq_csv:
-    ler = csv.reader(arq_csv)
+with open('arquivo_de_testes.in.csv', 'r') as arquivo_csv:
+    csv = csv.reader(arquivo_csv)
 
-    for linha in ler:
-        print(linha)    
+    print(arq_json['initial'])
+    print(arq_json['final'][1])
+    print(arq_json['transitions'][2]['to'])
