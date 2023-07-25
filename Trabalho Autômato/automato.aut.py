b55b1cc2b30250c0dc5178ed227b0b365f881a5a
Gustavo-Granadier <139660911+Gustavo-Granadier@users.noreply.github.com>
2023-07-25 19:19:36 -0300

    Update automato.aut.py

--- Trabalho Autômato/automato.aut.py
+++ Trabalho Autômato/automato.aut.py
@@ -9,10 +9,16 @@ arquivo2 = argvs[2]
 
 with open(arquivo1, 'r') as arquivo_json:
     arq_json = json.load(arquivo_json)
-
-with open(arquivo2, 'r') as arquivo_csv:
-    csv = csv.reader(arquivo_csv)
-
+    
     print(arq_json['initial'])
     print(arq_json['final'][1])
     print(arq_json['transitions'][2]['to'])
+    
+with open(arquivo2, 'r') as arquivo_csv:
+    arq_csv = csv.reader(arquivo_csv)
+    for line_number, content in enumerate(arq_csv): 
+        colunas = content.split(';')
+        p = list(f"{colunas[0]}")
+
+        for n in p:
+            print(n)
