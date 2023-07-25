bf8e6b16bae133d80979d12e23ace569a4a4940f
Gustavo-Granadier <139660911+Gustavo-Granadier@users.noreply.github.com>
2023-07-24 22:02:50 -0300

    Update automato.aut.py

--- Trabalho Autômato/automato.aut.py
+++ Trabalho Autômato/automato.aut.py
@@ -1,10 +1,16 @@
 import json
 import csv
+import sys
 
-with open('arquivo_do_automato.aut.json') as arquivo_json:
+argvs = sys.argv
+arquivo1 = argvs[1]
+arquivo2 = argvs[2]
+#arquivo3 = argvs[3]
+
+with open(arquivo1, 'r') as arquivo_json:
     arq_json = json.load(arquivo_json)
 
-with open('arquivo_de_testes.in.csv', 'r') as arquivo_csv:
+with open(arquivo2, 'r') as arquivo_csv:
     csv = csv.reader(arquivo_csv)
 
     print(arq_json['initial'])
