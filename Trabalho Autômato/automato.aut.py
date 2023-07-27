a810d862c102d84d8d73feac215070dc2e6ee800
Gustavo-Granadier <139660911+Gustavo-Granadier@users.noreply.github.com>
2023-07-27 16:47:10 -0300

    Update automato.aut.py

--- Trabalho Autômato/automato.aut.py
+++ Trabalho Autômato/automato.aut.py
@@ -14,11 +14,31 @@ with open(arquivo1, 'r') as arquivo_json:
     print(arq_json['final'][1])
     print(arq_json['transitions'][2]['to'])
     
-with open(arquivo2, 'r') as arq_csv:
-    ler = csv.reader(arq_csv)
-    for line_number, content in enumerate(arq_csv): 
-        colunas = content.split(';')
-        p = list(f"{colunas[0]}")
+# Lista para armazenar as strings da primeira coluna
+entradas = []
 
-        for n in p:
-            print(n)
+# Abre o arquivo CSV e lê os dados
+with open(arquivo2, "r") as csvfile:
+    # Criamos o leitor CSV
+    leitor = csv.reader(csvfile, delimiter=";")
+
+    # Itera sobre as linhas do arquivo
+    for linha in leitor:
+        # Adiciona a primeira string da coluna à lista
+        entradas.append(linha[0])
+
+
+# Inicializa um novo vetor que irá conter as palavras separadas
+entradas_separadas = []
+
+# Itera sobre cada palavra do vetor de strings
+for palavra in entradas:
+    # Separa cada letra da palavra e cria um novo vetor com as letras
+    entradas_palavras = list(palavra)
+    
+    # Adiciona o vetor de letras ao vetor_separado
+    entradas_separadas.append(entradas_palavras)
+
+# Exibe o resultado
+print(entradas_separadas)
+
